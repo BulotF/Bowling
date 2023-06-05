ec87d5af7fa287fb65ef41e971b97855917df7e2
Bulot François <f_bulot@hotmail.com>
2023-06-05 17:06:23 +0200

    initialize money-portfolio

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
-type Game = {
-  lancer: (int: number) => void;
-  score: number;
+export type Currency = "USD" | "EUR" | "KRW";
+
+export type Money = {
+  amount : number ;
+  currency : Currency;
+  multiply : (multiplier: number) => Money;
+  divide : (multiplier: number) => Money;
+  add : (money : Money) => Money;
+  exchange: (toCurrency : Currency) => Money;
+  toEqual: (money : Money) => boolean;
+  logAmount : () => string;
 };
-export function createGame(): Game {
+
+const EXCHANGE = [
+  {"from": "EUR" , "to" : "USD" , "rate" : 1.2     },
+  {"from": "USD" , "to" : "EUR" , "rate" : 0.82    },
+  {"from": "USD" , "to" : "KRW" , "rate" : 1100    },
+  {"from": "KRW" , "to" : "USD" , "rate" : 0.0009  },
+  {"from": "EUR" , "to" : "KRW" , "rate" : 1344    },
+  {"from": "KRW" , "to" : "EUR" , "rate" : 0.00073 }  
+]
+
+export function createMoney (amount: number, currency : Currency) : Money {
+
+  const multiply = (quantity: number) => createMoney (amount * quantity , currency);
+
+  const exchange = (toCurrency : Currency) => {
+    if (toCurrency === currency) return createMoney(amount, currency);
+    const exchangeRate = EXCHANGE.find(({from, to}) => from === currency && to === toCurrency)?.rate as number;
+    return createMoney(amount * exchangeRate , toCurrency);
+  }
+
+  const add = (money: Money) => {
+    return createMoney(amount + money.exchange(currency).amount , currency)
+  }
+
   return {
-    lancer: (int: number) => {},
-    score: 0,
-  };
+    amount : amount,
+    currency: currency,
+    multiply: multiply,
+    divide: (quantity: number) => multiply(1 / quantity),
+    add: add,
+    exchange: exchange,
+    toEqual: (money: Money) =>
+    money.amount === amount && money.currency === currency,
+    logAmount : () =>`${amount} ${currency}`
+  }
+
 }
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,15 +1,41 @@
-import { assert, describe, expect, it } from "vitest";
+import { assert, describe, it } from "vitest";
+import {createMoney} from "../src";
+import type { Currency, Money } from "../src";
 
-describe("suite name", () => {
-  it("foo", () => {
-    assert.equal(Math.sqrt(4), 2);
+describe("vérification des montants multiplié", () => {
+  const amount1 = {amount: 5 , currency : "USD" as Currency } ;
+  const amount2 = {amount: 10 , currency : "EUR"  as Currency} ;
+  const amount3 = {amount: 4002 , currency : "KRW"  as Currency} ;
+
+
+  it("5 USD x 2 = 10 USD", () => {
+    assert(createMoney(amount1.amount, amount1.currency).multiply(2).toEqual(createMoney(10,"USD")));
+  });
+   it("10 EUR x 2 = 20 EUR", () => {
+    assert(createMoney(amount2.amount, amount2.currency).multiply(2).toEqual(createMoney(20,"EUR")));
+  });
+  it("4002 KRW / 4 = 1000.5 KRW", () => {
+    assert(createMoney(amount3.amount, amount3.currency).divide(4).toEqual(createMoney(1000.5,"KRW")));
+  });
+});
+
+describe("vérification des conversions", () => {
+  const amount5USD = {amount: 5 , currency : "USD" as Currency } ;
+  const amount10USD = {amount: 10 , currency : "USD" as Currency } ;
+  const amount10EUR = {amount: 10 , currency : "EUR" as Currency } ;
+
+  it("5 USD + 10 USD = 15 USD", () => {
+    assert(createMoney(amount5USD.amount, amount5USD.currency).add(createMoney(amount10USD.amount, amount10USD.currency)).toEqual(createMoney(15,"USD")));
+  });
+  it("5 USD + 10 EUR = 17 USD", () => {
+    assert(createMoney(amount5USD.amount, amount5USD.currency).add(createMoney(amount10EUR.amount, amount10EUR.currency)).toEqual(createMoney(17,"USD")));
+  });
+  it("1 USD + 1100 KRW = 2200 KRW", () => {
+    assert.equal(createMoney(1, "USD").add(createMoney(1100, "KRW")).exchange("KRW").logAmount(),"2200 KRW");
   });
 
-  it("bar", () => {
-    expect(1 + 1).eq(2);
-  });
+  
 
-  it("snapshot", () => {
-    expect({ foo: "bar" }).toMatchSnapshot();
-  });
+
+
 });
